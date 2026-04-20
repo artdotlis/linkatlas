171dce29085997350188a75c58f5f001e85ea345
artdotlis <artdotlis@pm.me>
2026-04-20 11:14:58 +0200

    fix: prevent keyerror when validating configuration keys

--- packages/utilslink/src/utilslink/configs/agent.py
+++ packages/utilslink/src/utilslink/configs/agent.py
@@ -8,6 +8,6 @@ def create_agent_config(conf: Path) -> AgentConf:
     print(f"parsing {conf} - AGENT")
     with conf.open("rb") as fh_c:
         conf_d = tomllib.load(fh_c)
-        if "agent" not in conf_d and not isinstance(conf_d["agent"], dict):
+        if not("agent" in conf_d and isinstance(conf_d["agent"], dict)):
             raise BootstrapEx("no agent config found")
         return AgentConf(**conf_d["agent"])
--- packages/utilslink/src/utilslink/configs/keycloak.py
+++ packages/utilslink/src/utilslink/configs/keycloak.py
@@ -8,8 +8,8 @@ def create_lpsn_config(conf: Path) -> LPSNConf:
     print(f"parsing {conf} - LPSN")
     with conf.open("rb") as fh_c:
         conf_d = tomllib.load(fh_c)
-        if "dsmz_keycloak" not in conf_d and not isinstance(
+        if not("dsmz_keycloak" in conf_d and isinstance(
             conf_d["dsmz_keycloak"], dict
-        ):
+        )):
             raise BootstrapEx("no dsmz_keycloak config found")
         return LPSNConf(**conf_d["dsmz_keycloak"])
